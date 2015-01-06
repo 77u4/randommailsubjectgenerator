2a1345dcb56b52cc67594012d920430da98b45d5
77u4 <github@jh0.eu>
2015-01-06 20:50:46 +0100

    Added help subprogram/file

---
+++ help.lua
@@ -0,0 +1,12 @@
+-- WTF.
+print("============ Random Mail Subject Generator - Help ============")
+print("The random mail subject generator will return 3 suspicious strings\nby default unless you append arguments and parameters")
+print() -- I could have used \n, but this is way easier to read.
+print("USAGE")
+print("","lua rmsg.lua [-arguments] [parameters]")
+print()
+print("ARGUMENTS and PARAMETERS")
+print("","-h    --help                display help (that's what you see right now)")
+print("","-w    --words number        define the number of words generated")
+print("","-c    --category [String]   define the category of suspicious string. Will output all categories available if there is no category defined.")
+print("","-r    --random              same as evaluating program without parameters")
