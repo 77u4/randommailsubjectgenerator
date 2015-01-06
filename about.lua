c8434047ce99194d46c7a421b45608216bbe2253
77u4 <github@jh0.eu>
2015-01-06 22:48:39 +0100

    Added about.lua

---
+++ about.lua
@@ -0,0 +1,6 @@
+-- WTF.
+print("============ Random Mail Subject Generator - About ============")
+print("This Software is distributed under the GNU GENERAL PUBLIC LICENSE Version 3")
+print("and the code is available on GitHub under \nhttps://github.com/77u4/randommailsubjectgenerator")
+print("Happy cloning, forking and modifying!")
+print()
