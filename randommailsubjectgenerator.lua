2252cad4ea774617b5530ffabd618e8989826722
77u4 <github@jh0.eu>
2015-01-06 20:02:44 +0100

    Added mian functions and what they are supposed to be like.

--- randommailsubjectgenerator.lua
+++ randommailsubjectgenerator.lua
@@ -1,3 +1,48 @@
---[[--
-WTF.
---]]--
+-- WTF.
+function getCategories()
+	--[[--
+	Gets all available categories from the json file.
+
+	@Return: Table
+		list of all categories available
+	--]]--
+end
+
+function getStringCount(category)
+	--[[--
+	Gets the number of strings in $category.
+	
+	@Parameter: category
+		Category whose strings shall be counted
+
+	@Return: number
+		Number of strings in $category. not null.
+	--]]--
+end
+
+function getString(category, num)
+	--[[--
+	Loads $num string from $category.
+	
+	@Parameter: category
+		Category in which the string is located
+
+	@Parameter: num
+		Position of the string.
+	
+	@Return: String
+	--]]--
+end
+
+function getMailSubject(category)
+	--[[-- 
+	Generates a mail subject. If category is empty, it will choose one randomly.
+
+	@Parameter: category
+		String -- see getCategories()
+	
+	@Return: String
+		2 - 5 keywords that'll help you to defend your ranking on the terror list.
+	--]]--
+
+end
